fbd47a8e0d96d9ce8f49133cfb2e2141d9d57968
Anmol Pathry <Anmol@MacBook-de-Anmol.local>
2018-10-22 09:40:32 -0500

    Terminados los discos de Shawn y Kongos

--- src/pages/kongos-canciones/kongos-canciones.ts
+++ src/pages/kongos-canciones/kongos-canciones.ts
@@ -15,6 +15,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class KongosCancionesPage {
 
+  k;
+  kotracks =["1.Come With Me Now (3.32)", "2.Sex On the Radio (3.57)", "3.Escape(4.34)",
+   "4.Kids These Days (3.56)", "5.As We Are (4.42)", "6. I Want To Know (3.55)", "7. Hey I Don't Know (4.01)",
+  "8.Traveling On (4.32)", "9.Take Me Back (4.46)", "10.It's A Good Life (3.59)" ,"11.I'm Only Joking (3.45)", 
+  "12. This Time I Won't (5.46)"
+  ];
+
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
--- src/pages/shawn-canciones/shawn-canciones.ts
+++ src/pages/shawn-canciones/shawn-canciones.ts
@@ -15,6 +15,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShawnCancionesPage {
 
+  s;
+  shtracks =["1.Ruin (4.01)", "2.Mercy (3.28)", "3.Treat You Better (3.07)",
+   "4.Three Empty Words (3.19)", "5.Don't Be a Fool (3.35)", "6.Like This (3.06)", "7.No Promises(2.46)",
+  "8.Lights On (3.06)", "9.Honest (3.19)", "10.Patience (2.55)" ,"11.Bad Reputation (3.18)", "12. Understand (5.00)"
+  , "13. There's Nothing Holdin' Me Back (3.14)"
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
